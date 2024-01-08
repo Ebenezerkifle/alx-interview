5ebdf072320b3c7482c28739bd83c503039d53d4
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-08 04:11:55 +0300

    pascal triangle problem solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,15 +7,21 @@ def pascal_triangle(n):
     for x in range(1, n+1):
         inner = []
         for y in range(1, x+1):
-            inner.append(y)
+            if(y == 1 or y == x):
+                inner.append(1)
+            else:
+                ## look for the previous elements.
+                temp = triangle[x-2]
+                value = temp[y-2]+temp[y-1]
+                inner.append(value)
         triangle.append(inner)
     return triangle
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+# def print_triangle(triangle):
+#     """
+#     Print the triangle
+#     """
+#     for row in triangle:
+#         print("[{}]".format(",".join([str(x) for x in row])))
 
-print_triangle(pascal_triangle(5))
+# print_triangle(pascal_triangle(19))
