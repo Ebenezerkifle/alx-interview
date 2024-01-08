f207bde958736a4a72c76f3b2251b17fb5daff7f
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-08 04:24:03 +0300

    pascal triangle problem solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,15 +7,17 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = []
+
     for x in range(1, n+1):
         """This loop keeps track of the size of the triangle."""
         inner = []
+
         for y in range(1, x+1):
             """This loop is where the real magic happens."""
             if(y == 1 or y == x):
                 inner.append(1)
             else:
-                ## look for the previous elements.
+                """ look for the previous elements. """
                 temp = triangle[x-2]
                 value = temp[y-2] + temp[y-1]
                 inner.append(value)
