d1c05720bda382447d574f86a9f8ce6c8f282928
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-08 04:17:19 +0300

    some comments added

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+""" This is a simple method to solve a pascal traingle problem. """
 
 def pascal_triangle(n):
+    """if the given n value is less or equals 1
+    the function returns empty list."""
     if n <= 0:
         return []
     triangle = []
     for x in range(1, n+1):
+        """This loop keeps track of the size of the triangle."""
         inner = []
         for y in range(1, x+1):
+            """This loop is where the real magic happens."""
             if(y == 1 or y == x):
                 inner.append(1)
             else:
