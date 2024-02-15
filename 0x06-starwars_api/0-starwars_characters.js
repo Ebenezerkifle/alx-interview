d3f1e0ba21babbd26b8b8d0bb157dbd400fef9e5
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-02-15 20:24:38 +0300

    starwars character api is added

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,24 @@
 /**
  * Starwars Characters
  */
-
 const request = require('request');
-const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(movieUrl, (error, res, body) => {
-   if (error) console.log(err);
-   const index = 0;
-   const characters = JSON.parse(body).characters;
-   printMovieCharacter(characters, index);
+const movieId = process.argv[2];
+const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(movieURL, (err, res, body) => {
+  if (err) console.log(err);
+  const index = 0;
+  const characters = JSON.parse(body).characters;
+  printMovieCharacter(characters, index);
 });
 
-const printMovieCharacter = (url, i) => {
-    request(url[i], (err, res, body) => {
-        if (err) console.log(err);
-        console.log(JSON.parse(body).name);
-        if (++i < url.length) {
-          printMovieCharacter(url, i++);
-        }
-      });
-}
+const printMovieCharacter = function (url, i) {
+  request(url[i], (err, res, body) => {
+    if (err) console.log(err);
+    console.log(JSON.parse(body).name);
+    if (++i < url.length) {
+      printMovieCharacter(url, i++);
+    }
+  });
+};
